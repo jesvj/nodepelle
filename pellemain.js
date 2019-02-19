c98965964b90e2626a4a4f871cba4f95d96e34d9
Jes Vestervang Jensen <jes.vestervang@gmail.com>
2019-02-19 14:20:55 +0100

    Working upload to thingspeak via http get instead of thingspeakclient which would not work for some reason

--- pellemain.js
+++ pellemain.js
@@ -1,7 +1,7 @@
 const config = require('config');
+const https = require('https');
 // Use export NODE_ENV=config_sample before calling node pellemain.js
 
-//console.log('chId: ' + config.get('ThingSpeak.channelId') + ', config.get('ThingSpeak.apiRead'): ' + config.get('ThingSpeak.apiRead') + 'config.get('ThingSpeak.apiWrite'): ' + config.get('ThingSpeak.apiWrite'))
 
 var can = require('socketcan');
 var fs = require('fs');
@@ -9,41 +9,36 @@ var channel = can.createRawChannel("can0", true);
 var network = can.parseNetworkDescription("igneo.kcd");
 var db_main = new can.DatabaseService(channel, network.buses["Main"]);
 
-var ThingSpeakClient = require('thingspeakclient');
-var client = new ThingSpeakClient();
+let tempCalc = require('./temperature')
+
+var apiReadKey = config.get('ThingSpeak.apiRead').trim();
+var apiWriteKey = config.get('ThingSpeak.apiWrite').trim();
+var channelId = config.get('ThingSpeak.channelId').trim();
 
 
+console.log('channelId  is ' + channelId +  ', apiReadKey:' + apiReadKey + ', apiWriteKey:' + apiWriteKey );
 
-console.log('channelId  is ' + config.get('ThingSpeak.channelId'));
-
-client.attachChannel(config.get('ThingSpeak.channelId'), { writeKey:config.get('ThingSpeak.apiWrite'), readKey:config.get('ThingSpeak.apiRead')}, function(err, resp)
-{
-    if (!err && resp > 0)
-    {
-        console.log('attached successfully. response was: ' + resp);
-    }
-});
-
-// Log any message
+// Log any message, comment in for debugging etc
 //channel.addListener("onMessage", function(msg) { console.log(msg); } );
 
 channel.start();
 
-db_main.messages["FanSpeed"].signals["FanSpeed"].onChange(function(s) {
-   console.log("FanSpeed " + s.value);
-   client.updateChannel(config.get('ThingSpeak.channelId'), {field8: s.value}, function(err, resp)
-   {
-       if (!err && resp > 0)
-       {
-           console.log('update successfully. Entry number was: ' + resp);
-       }
-   });
-});
+db_main.messages["FanSpeed"].signals["FanSpeed"].onChange(function(s)
+  {
+    console.log("FanSpeed " + s.value);
+    sendDataTS(channelId, "field8", apiWriteKey, s.value);
+  });
 
-db_main.messages["Temp1"].signals["BoilerTemp"].onChange(function(s) {
-   console.log("BoilerTemp " + s.value);
+db_main.messages["Temp1"].signals["BoilerTemp"].onUpdate(function(s) {
+  temperature = tempCalc.temp('CT2a', s.value);
+  console.log("BoilerTemp " + temperature);
 });
 
 db_main.messages["Feeder"].signals["FeederState"].onChange(function(s) {
    console.log("FeederState " + s.value);
 });
+
+function sendDataTS(chId, fieldId, apiWriteKey, val)
+{
+    https.get('https://api.thingspeak.com/update.json?api_key' + '=' + apiWriteKey + '&' + fieldId + '=' +  val);
+}
